dc9870485f3cf806549d15546b3d2d13ed8c4665
john <jmlovitsch@gmail.com>
2021-01-11 09:39:47 -0600

    initial goals input box created and formatted

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.views}>
+        <TextInput placeholder="Enter Goal" style={styles.input} />
+        <Button title="+" style={styles.button} />
+      </View>
+      <View></View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    borderBottomColor: "black",
+    borderWidth: 2,
+    padding: 10,
+    width: "80%"
+  },
+  views: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    // alignItems: 'center',
+  },
+  button: {
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
